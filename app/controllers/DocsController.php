9417c7866111c59f246efeae92b119c0e6326c25
Far Tseng <farrrr@gmail.com>
2013-12-23 23:30:14 +0800

    update status / docs for change versions
    Signed-off-by: Far Tseng <farrrr@gmail.com>

--- app/controllers/DocsController.php
+++ app/controllers/DocsController.php
@@ -17,16 +17,45 @@ class DocsController extends BaseController
 
     public function getDocs($document)
     {
+        if (in_array($document, array('4-0', '4-1', 'dev'))) {
+            switch($document) {
+            case '4-0':
+                $docs_ver = '4.0';
+                break;
+            case '4-1':
+                $docs_ver = '4.1';
+                break;
+            case 'dev':
+                $docs_ver = 'dev';
+                break;
+            }
+            Session::put('doc-ver', $docs_ver);
 
-        $docs_ver = '4.1';
+            return Redirect::back();
+        }
+
+        switch(Session::get('doc-ver', '4.1')) {
+            case '4.0':
+                $docs_ver = '4.0';
+                break;
+            case 'dev':
+                $docs_ver = 'dev';
+                break;
+            default: // include 4.1
+                $docs_ver = '4.1';
+                break;
+        }
         $docs_md = app_path() . '/docs.md/' . $docs_ver . '/' . $document . '.md';
 
-        if (!file_exists($docs_md)) return 'Error!!';
+        if (!file_exists($docs_md)) {
+            return Redirect::to('docs/introduction');
+        }
 
         $markdownParser = new MarkdownParser();
         $docs_content = $markdownParser->transformMarkdown(file_get_contents($docs_md));
 
         return View::make('docs/index')
+            ->with('docs_ver', $docs_ver)
             ->with('docs_content', $docs_content);
     }
 }
--- app/views/docs/index.blade.php
+++ app/views/docs/index.blade.php
@@ -10,9 +10,9 @@
         <!-- version -->
         <div id="version">
             <ul class="nolist">
-                <li><a href="/docs/dev" title="Dev">Dev</a></li>
-                <li class="current"><a href="/docs/4-1" title="4.1">4.1</a></li>
-                <li><a href="/docs/4-0" title="4.0">4.0</a></li>
+                <li @if($docs_ver == 'dev') class="current" @endif><a href="/docs/dev" title="Dev">Dev</a></li>
+                <li @if($docs_ver == '4.1') class="current" @endif><a href="/docs/4-1" title="4.1">4.1</a></li>
+                <li @if($docs_ver == '4.0') class="current" @endif><a href="/docs/4-0" title="4.0">4.0</a></li>
             </ul>
         </div>
         <!-- /version -->
