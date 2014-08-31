7b38a68b3304d8f77e80b599ff613a2fdc2a38b7
Bo-Yi Wu <appleboy.tw@gmail.com>
2014-08-31 21:19:59 +0800

    update title
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- app/views/docs/index.blade.php
+++ app/views/docs/index.blade.php
@@ -1,36 +1,35 @@
 @extends('layouts.default')
 
 @section('header_boxed')
-        <!-- tagline -->
-        <div id="tagline">
-            <h1>中文文件.</h1>
-        </div>
-        <!-- /tagline -->
+<!-- tagline -->
+<div id="tagline">
+    <h1>繁體中文文件</h1>
+</div>
+<!-- /tagline -->
 
-        <!-- version -->
-        <div id="version">
-            <ul class="nolist">
-                <li @if($docs_ver == 'dev') class="current" @endif><a href="/docs/dev" title="Dev">Dev</a></li>
-                <li @if($docs_ver == '4.2') class="current" @endif><a href="/docs/4-2" title="4.2">4.2</a></li>
-                <li @if($docs_ver == '4.1') class="current" @endif><a href="/docs/4-1" title="4.1">4.1</a></li>
-                <li @if($docs_ver == '4.0') class="current" @endif><a href="/docs/4-0" title="4.0">4.0</a></li>
-            </ul>
-        </div>
-        <!-- /version -->
+<!-- version -->
+<div id="version">
+<ul class="nolist">
+    <li @if($docs_ver == 'dev') class="current" @endif><a href="/docs/dev" title="Dev">Dev</a></li>
+    <li @if($docs_ver == '4.2') class="current" @endif><a href="/docs/4-2" title="4.2">4.2</a></li>
+    <li @if($docs_ver == '4.1') class="current" @endif><a href="/docs/4-1" title="4.1">4.1</a></li>
+    <li @if($docs_ver == '4.0') class="current" @endif><a href="/docs/4-0" title="4.0">4.0</a></li>
+</ul>
+</div>
+<!-- /version -->
 @stop
 
 @section('content')
-            <!-- docs -->
-            <section id="documentation">
-                <article class="boxed">
-                @include('docs/sidebar')
-                    <!-- docs content -->
-                    <div id="docs-content">
-                    {{ $docs_content }}
-                    </div>
-                    <!-- /docs content -->
-                </article>
-    	    </section>
-    	    <!-- /docs -->
+<!-- docs -->
+<section id="documentation">
+    <article class="boxed">
+    @include('docs/sidebar')
+        <!-- docs content -->
+        <div id="docs-content">
+        {{ $docs_content }}
+        </div>
+        <!-- /docs content -->
+    </article>
+</section>
+<!-- /docs -->
 @stop
-
